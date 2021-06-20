3045a2f3105808e26827fe06a0af71869730484f
Wedhimd <wedhimd@gmail.com>
2021-06-20 11:09:34 +0200

    add static files and refactor all the project

--- wp-content/themes/Qolzum/header.php
+++ wp-content/themes/Qolzum/header.php
@@ -1,10 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html dir="rtl" lang="ar">
+
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="One stop for all the Horn of Africa news">
+    <title> Qolzum - القلزم </title>
     <?php wp_head(); ?>
+
 </head>
-<body>
-    
+
+<body <?php body_class() ?>>
+
+    <header class="sticky-top">
+        <?php
+        wp_nav_menu(
+            array(
+                "theme_location" => "top-menu",
+                "menu_class" => "navigation"
+            )
+        )
+        ?>
+    </header>
