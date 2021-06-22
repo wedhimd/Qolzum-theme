e87a5f5c5abd4546c2698d64a1755c96912dbe82
Wedhimd <wedhimd@gmail.com>
2021-06-22 17:39:30 +0200

    back to bootstrap 4.0.0 and finish front-page

--- wp-content/themes/Qolzum/front-page.php
+++ wp-content/themes/Qolzum/front-page.php
@@ -1,9 +1,121 @@
 <?php get_header(); ?>
-<div class="container pt-5 pb-5">
-    <h1><?php the_title() ?></h1>
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <?php the_content(); ?>
-    <?php endwhile;
-    endif; ?>
-</div>
+
+<main>
+    <article class="popular-news">
+        <div class="featured">
+            <h2 class="thumbnail-headings">آخر اﻷخبار</h2>
+            <section class="popular-news-carousel">
+                <div id="carousel" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="d-block w-100" src="<?php echo get_theme_file_uri("/assets/images/carousel1.jpg") ?>" alt="First slide">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>ينا الألم. في بعض الأحيان ونظراً للالتزامات التي يفرضها علينا الواجب والعمل سنتنازل غالباً ونرفض الشعور</h5>
+                                <p>11th Jan 2019</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="<?php echo get_theme_file_uri("/assets/images/carousel2.jpg") ?>" alt="Second slide">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>لآخر نشجب ونستنكر هؤلاء الرجال المفتونون بنشوة اللحظة الهائمون في رغبات </h5>
+                                <p>11th Jan 2019</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="<?php echo get_theme_file_uri("/assets/images/carousel3.jpg") ?>" alt="Third slide">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>إما أن يرفض مصادر السعادة من أجل ما هو أكثر أهمية أو يتحمل الألم من أجل ألا يتحمل ما هو أسو. قب أليمة أو آخر أراد أن يتجنب الألم الذ </h5>
+                                <p>11th Jan 2019</p>
+                            </div>
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </section>
+
+        </div>
+        <div id="latest">
+            <h2 class="thumbnail-headings">الاقتصاد</h2>
+            <section class="news">
+                <div class="news-container">
+                    <img src="<?php echo get_theme_file_uri("/assets/images/carousel1.jpg") ?>">
+                    <p class="carousel-text">لكن لا بد أن أوضح لك أن كل هذه الأفكار المغلوطة حول استنكار النشوة وتمجيد الألم نشأت بالفعل،</p>
+                </div>
+            </section>
+            <section class="news">
+                <div class="news-container">
+                    <img src="<?php echo get_theme_file_uri("/assets/images/carousel2.jpg") ?>">
+                    <p class="carousel-text">تنكر هؤلاء الرجال المفتونون بنشوة اللحظة الهائمون في</p>
+                </div>
+            </section>
+        </div>
+        <div id="our-picks">
+            <h2 class="thumbnail-headings">الرياضة</h2>
+            <section class="news">
+                <div class="news-container">
+                    <img src="<?php echo get_theme_file_uri("/assets/images/carousel3.jpg") ?>">
+                    <p class="carousel-text"> الرجال المفتونون بنشوة اللحظة الهائمون في رغباتهم فلا يدركون ما يعقبها من الألم والأسي المحتم، </p>
+                </div>
+            </section>
+            <section class="news">
+                <div class="news-container">
+                    <img src="<?php echo get_theme_file_uri("/assets/images/carousel1.jpg") ?>">
+                    <p class="carousel-text">لكن لا بد أن أوضح لك أن كل هذه الأفكار المغلوطة حول استنكار النشوة وتمجيد الألم نشأت بالفعل،</p>
+                </div>
+            </section>
+        </div>
+    </article>
+    <section class="more-news">
+        <div class="news-section">
+            <article class="world">
+                <h2 class="thumbnail-headings">العالم</h2>
+                <img src="<?php echo get_theme_file_uri("/assets/images/world1.jpg") ?>">
+                <h3>المغلوطة حول استنكار النشوة وتمجيد</h3>
+                <p>نستشعرها بصورة أكثر عقلانية ومنطقية فيعرضهم هذا لمواجهة الظروف الأليمة، وأكرر بأنه لا يوجد من يرغب في الحب ونيل المنال ويتلذذ بالآلام، الألم هو الألم ولكن نتيجة لظروف ما قد تكمن </p>
+            </article>
+            <article class="sport">
+                <h2 class="thumbnail-headings">الرياضة</h2>
+                <img src="<?php echo get_theme_file_uri("/assets/images/sport1.jpg") ?>">
+                <h3>الألم نشأت بالفعل، وسأعرض لك التفاصيل لتكتشف حقيقة وأساس تلك السعادة البشرية</h3>
+                <p> سأعرض مثال حي لهذا، من منا لم يتحمل جهد بدني شاق إلا من أجل الحصول على ميزة أو فائدة؟ ولكن من لديه الحق أن ينتقد شخص ما أراد أن يشعر بالسعادة التي لا تشوبها عواقب أليمة أو آخر أراد أن يتجنب الألم الذي ربما تنجم عنه بعض المتعة </p>
+            </article>
+        </div>
+
+        <aside>
+            <h4 class="thumbnail-headings">اﻷحدث</h4>
+            <div class="recent-news">
+                <img src="<?php echo get_theme_file_uri("/assets/images/article1.jpg") ?>">
+                <p>نتيجة لظروف ما قد تكمن السعاده فيما نتحمله م</p>
+            </div>
+            <div class="recent-news">
+                <img src="<?php echo get_theme_file_uri("/assets/images/aside2.jpg") ?>">
+                <p>نتيجة لظروف ما قد تكمن السعاده فيما نتحمله م</p>
+            </div>
+            <div class="recent-news">
+                <img src="<?php echo get_theme_file_uri("/assets/images/aside3.jpg") ?>">
+                <p>نتيجة لظروف ما قد تكمن السعاده فيما نتحمله م</p>
+            </div>
+            <div class="recent-news">
+                <img src="<?php echo get_theme_file_uri("/assets/images/aside4.jpg") ?>">
+                <p>نتيجة لظروف ما قد تكمن السعاده فيما نتحمله م</p>
+            </div>
+            <div class="recent-news">
+                <img src="<?php echo get_theme_file_uri("/assets/images/aside5.jpg") ?>">
+                <p>نتيجة لظروف ما قد تكمن السعاده فيما نتحمله م</p>
+            </div>
+        </aside>
+    </section>
+</main>
 <?php get_footer() ?>
