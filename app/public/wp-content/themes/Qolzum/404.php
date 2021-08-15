b407f04d199f9a486d39ae4ec25856511c86e169
Mohammed Ali <wedhimd@gmail.com>
2021-08-15 11:35:50 +0200

    add 404 page markup

---
+++ app/public/wp-content/themes/Qolzum/404.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<div class="container mt-5 mb-5" style="background-color: rosybrown;">
+    <div  style="margin-left: auto; margin-right: auto; width: 8rem">
+        <p style="text-align: center; font-size: 36px; width: 500px">page not found</p>
+        <h1 style="text-align: center; font-size: 75px">404</h1>
+        <div class="btn btn-success mb-5" style="width: 150px; text-align:center">go back</div>
+    </div>
+</div>
+
+<?php get_footer() ?>
