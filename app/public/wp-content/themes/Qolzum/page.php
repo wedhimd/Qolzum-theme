7ded0c562b27c0d3e1076b7a8205c88a9e6e99ba
Mohammed Ali <wedhimd@gmail.com>
2021-08-30 14:11:20 +0200

    add home and page sidebar template

--- app/public/wp-content/themes/Qolzum/page.php
+++ app/public/wp-content/themes/Qolzum/page.php
@@ -3,9 +3,9 @@
     <div class="container mt-4">
         <div class="row">
             <div class="col-8">
-            <h3 class="page__title block-title">
+                <h3 class="page__title block-title">
                 <span><?php  wp_title($sep=" ")?> </span>
-            </h3>
+                </h3>
                 <?php
                     // Start our WP Query
                     if (have_posts()) : while (have_posts()) : the_post(); 
@@ -23,58 +23,7 @@
                 <?php  endwhile; endif;?>                                
             </div>
             <div class="col-4">
-                <!-- most recet widget start -->
-                <div class="widget">
-                    <h3 class="widget-title">آخر اﻷخبار</h3>
-                    <?php  $recentNews = new WP_Query(array(
-                        // Define our WP Query Parameters
-                        'post_type' => 'post',
-                        'post_status' => 'publish',
-                        'category_name' => 'news',
-                        'meta_query' => array(array('key' => '_thumbnail_id')),
-                        "posts_per_page" => 4,
-                    )); ?>
-
-                    <?php 
-                        // Start our WP Query
-                        while ($recentNews -> have_posts()) : $recentNews -> the_post(); 
-                        // Display the Post Title with Hyperlink
-                    ?>
-
-                    <div class="widget-content">
-                        <ul class="latest-news">
-                            <li>
-                                <div class="latest-news-item clearfix">
-                                    <div class="item-thumbnail">
-                                    <a href="<?php the_permalink()?>">
-                                            <?php if (has_post_thumbnail()) : ?>
-                                            <img src="<?php the_post_thumbnail_url($size = "smallest"); ?>" class="latest-news-img">
-                                            <?php endif; ?>
-                                        </a>
-                                    </div>
-                                    <div class="news-item-container">
-                                        <h2 class="news-title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-                                        <p class="news-exerpt">
-                                            <?php if(has_excerpt()){
-                                                echo get_the_excerpt();
-                                            } else {
-                                                echo wp_trim_words(get_the_content(), 18);
-                                            } ?> <a href="<?php the_permalink(); ?>">...  المزيد</a>
-                                        </p>
-                                        <div class="news-meta">
-                                        <span class="news-date"> <?php the_modified_date('j  F,  Y'); ?> م</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                    <?php 
-                        // Repeat the process and reset once it hits the limit
-                        endwhile;
-                        wp_reset_postdata();
-                    ?>
-                </div> <!-- most recet widget end -->
+                <?php get_sidebar("page"); ?>
             </div>
             
         </div>
