5dfe933fa298ee4c8c512a6007b7c2dd33bf7c16
Mohammed Ali <wedhimd@gmail.com>
2021-09-03 18:05:05 +0200

    test php7.4.1 for the theme

--- app/public/wp-content/themes/Qolzum/tag.php
+++ app/public/wp-content/themes/Qolzum/tag.php
@@ -7,23 +7,24 @@
                 <span><?php single_tag_title() ?> </span>
                 </h3>
                 <div class="cards">
-                <?php
-                    // Start our WP Query
-                    if (have_posts()) : while (have_posts()) : the_post(); 
-                ?>
-            
-                <a href="<?php the_permalink() ?>" class="card">
-                        <div class="card__image" style="background-image: url('<?php echo the_post_thumbnail_url(get_the_ID())?>');"></div>
-                        <div class="card__content">
-                            <div class="card__title"><?php the_title() ?></div>
-                            <p class="card__snippet">
-                                <?php  echo wp_trim_words(get_the_content(), 18);?>
-                            </p>
-                        <div class="card__readmore">أكمل القراءة</div>
-                    </div>
-                </a>
-                <?php  endwhile; endif;?>                                
+                    <?php
+                        // Start our WP Query
+                        if (have_posts()) : while (have_posts()) : the_post(); 
+                    ?>
+                
+                    <a href="<?php the_permalink() ?>" class="card">
+                            <div class="card__image" style="background-image: url('<?php echo the_post_thumbnail_url(get_the_ID())?>');"></div>
+                            <div class="card__content">
+                                <div class="card__title"><?php the_title() ?></div>
+                                <p class="card__snippet">
+                                    <?php  echo wp_trim_words(get_the_content(), 18);?>
+                                </p>
+                            <div class="card__readmore">أكمل القراءة</div>
+                        </div>
+                    </a>
+                    <?php  endwhile; endif;?>                                
                 </div>
+                
             </div>
             <div class="col-lg-4 col-md-12">
                 <?php get_sidebar("page"); ?>
