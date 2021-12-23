1c9342320e546d75b229a1a69d1d1f50cb515199
wedhimd <wedhimd@gmail.com>
2021-12-23 22:48:29 +0200

    change category post counter links

--- themes/qolzum/inc/template-parts/category-widget.php
+++ themes/qolzum/inc/template-parts/category-widget.php
@@ -4,14 +4,15 @@
     <div class="widget-content">
         <div class="categories">
             <ul>
-                <li><a href="/news/politics/"> سياسة</a><span><?php echo count_cat_post("politics"); ?></span></li>
-                <li><a href="/news/politics/local/"> القرن الإفريقي</a><span><?php echo count_cat_post("local"); ?></span></li>
-                <li><a href="/news/politics/world/"> العالم</a><span><?php echo count_cat_post("world"); ?></span></li>
-                <li><a href="/news/economy/"> إقتصاد</a><span><?php echo count_cat_post("economy"); ?></span></li>
-                <li><a href="/news/sports/"> رياضة</a><span><?php echo count_cat_post("sports"); ?></span></li>
-                <li><a href="/news/science/"> العلوم والتكنلوجيا</a><span><?php echo count_cat_post("science"); ?></span></li>
-                <li><a href="/news/culture/"> الثقافة والفنون</a><span><?php echo count_cat_post("culture"); ?></span></li>
-                <li><a href="/news/miscellaneous/"> منوعات </a><span><?php echo count_cat_post("miscellaneous"); ?></span></li>
+                <li><a href="category/news/"> الأخبار</a><span><?php echo count_cat_post("news"); ?></span></li>
+                <li><a href="category/politics/"> سياسة</a><span><?php echo count_cat_post("politics"); ?></span></li>
+                <li><a href="category/politics/local/"> القرن الإفريقي</a><span><?php echo count_cat_post("local"); ?></span></li>
+                <li><a href="category/politics/world/"> العالم</a><span><?php echo count_cat_post("world"); ?></span></li>
+                <li><a href="category/economy/"> إقتصاد</a><span><?php echo count_cat_post("economy"); ?></span></li>
+                <li><a href="category/sports/"> رياضة</a><span><?php echo count_cat_post("sports"); ?></span></li>
+                <li><a href="category/science/"> العلوم والتكنلوجيا</a><span><?php echo count_cat_post("science"); ?></span></li>
+                <li><a href="category/culture/"> الثقافة والفنون</a><span><?php echo count_cat_post("culture"); ?></span></li>
+                <li><a href="category/miscellaneous/"> منوعات </a><span><?php echo count_cat_post("miscellaneous"); ?></span></li>
             </ul>
         </div>
     </div>
